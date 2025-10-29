39400dcdbbcff25a301eaa4babf5addb4ce0e89d
macheng <1826204647@qq.com>
2025-10-29 09:21:03 +0800

    main.c

--- main.c
+++ main.c
@@ -2,18 +2,43 @@
 #include<unistd.h>
 #include<sys/wait.h>
 #include<stdlib.h>
+
 int data;
 
 int main()
 {
+
     pid_t pid = fork();
     if(pid>0){
         printf("parent pid = %d...\n",getpid());
         printf("parent data = %d,&data = %p...\n",data,&data);
         int status;
-        pid_t wait_pid = wait(&status);
-        if(WIFEXITED(status)){
-            printf("子进程正常退出，退出状态码：%d..\n",WEXITSTATUS(status));
+        while(1){
+        pid_t wait_pid = waitpid(pid,&status,WNOHANG);
+        if(wait_pid == -1)
+        {
+            printf("wait failed\n");
+            break;
+        }
+        else if(wait_pid == 0)
+        {
+            printf("son progress exe...\n");
+            sleep(1);
+            continue;
+        }
+        else
+        {
+            // ⼦进程已结束
+            printf("\nParent: Child [%d] has finished!\n", pid);
+            if (WIFEXITED(status)) {
+                printf("Parent: Child exited normally with status: %d\n",
+WEXITSTATUS(status));
+                } else if (WIFSIGNALED(status)) {
+                    printf("Parent: Child was terminated by signal: %d\n",
+WTERMSIG(status));
+            }
+            break;
+                }
             }
     }
     else if(pid == 0){
